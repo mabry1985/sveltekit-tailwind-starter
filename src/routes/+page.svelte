c40bf9d28e8485f808aaf9d5a24a38c59fb6b5fa
Josh Mabry <mabry1985@gmail.com>
2022-10-11 01:47:27 -0700

    adds example api call

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p class="underline text-green-900">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+const response = async () => {
+		var response = await fetch('http://localhost:7860/api/artists/63452559b9894c4f8c8b3d2b');
+		var result = await response.json();
+    console.log(result);
+		return result;
+	}
+
+const data = response()
+
+</script>
+
+<h1 class="text-lg">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{#await data}
+	<h2>Loading....</h2>
+{:then user}
+<p>{user.firstName}</p>
+<p>{user.lastName}</p>
+{:catch err}
+	<h2>Error while loading the data</h2>
+{/await}
