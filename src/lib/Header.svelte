63d7ff39452bfae8177ebf23c3ff82b891a0a57a
Josh Mabry <mabry1985@gmail.com>
2022-10-18 21:10:46 -0700

    scaffolds header

---
+++ src/lib/Header.svelte
@@ -0,0 +1,42 @@
+<script>
+
+</script>
+
+<header>
+  <div class="wrapper">
+    <div>
+      <a href="/">
+        <!-- <img src="/images/oddball-logo.svg" alt="Oddball Logo" /> -->
+        <h1>Oddball</h1>
+      </a>
+    </div>
+    <div class="flex flex-row">
+      <nav >
+        <ul >
+          <li><a href="/artists" aria-label="Artists information">Artists</a></li>
+          <li><a href="/about" aria-label="Information about the shop">About</a></li>
+          <li><a href="https://shop.oddballstudios.com/" target="_blank" referrerpolicy="no-referrer" aria-label="visit the merchandise store">Shop</a></li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</header>
+
+<style>
+  .wrapper {
+  padding: 15px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+h1 {
+  font-weight: 900;
+  font-size: 20px;
+  line-height: 1;
+  margin: 6px 0 6px 10px;
+  display: inline-block;
+  vertical-align: top;
+}
+
+</style>
